9546c9963e101542b429ef43f600c4888a5caa82
Muhammad Rivaldi <rivaldimuhammad4897@gmail.com>
2021-08-19 11:23:38 +0900

    mencoba buat halaman home

---
+++ app/Views/home.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+
+    <!-- style -->
+    <link rel="stylesheet" href="assets/main/css/jquery-ui.css">
+    <link rel="stylesheet" href="assets/main/css/bootstrap.min.css">
+    <link rel="stylesheet" href="assets/main/css/lightbox.min.css">
+    <link rel="stylesheet" href="assets/main/css/owl.carousel.min.css">
+    <link rel="stylesheet" href="assets/main/css/normalize.css">
+    <link rel="stylesheet" href="assets/main/css/slicknav.min.css">
+    <link rel="stylesheet" href="assets/main/css/style.css">
+    <link rel="stylesheet" href="assets/main/css/responsive.css">
+    <link rel="stylesheet" href="assets/main/css/chosen.css">
+    <link rel="stylesheet" href="assets/main/css/datatable.min.css">
+    <!-- end style -->
+
+    <title>Home</title>
+</head>
+
+<body>
+
+    <!-- preloader -->
+    <div id="preloader">
+        <div id="status" style="background-image: url(assets/main/img/preloader/preloader.gif)"></div>
+    </div>
+    <!-- end preloader -->
+
+    <!-- navbar -->
+    <div class="menu-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-4 col-sm-12 col-xs-12">
+                    <div class="logo">
+                        <a href="/">Donor</a>
+                    </div>
+                </div>
+                <div class="col-md-8 col-sm-9">
+                    <div class="menu">
+                        <ul id="nav" class="main-menu">
+                            <li>
+                                <a href="/">Home</a>
+                            </li>
+                            <li>
+                                <a href="#">Donor</a>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end navbar -->
+
+    <!-- content -->
+    <div class="container">
+        <h2>halo</h2>
+    </div>
+    <!-- end content -->
+
+    <!-- footer -->
+    <script src="/assets/main/js/jquery-2.2.4.min.js"></script>
+    <script src="/assets/main/js/jquery-ui.js"></script>
+    <script src="/assets/main/js/bootstrap.min.js"></script>
+    <script src="/assets/main/js/chosen.jquery.js"></script>
+    <script src="/assets/main/js/docsupport/init.js"></script>
+    <script src="/assets/main/js/lightbox.min.js"></script>
+    <script src="/assets/main/js/jquery.dataTables.min.js"></script>
+    <script src="/assets/main/js/owl.carousel.min.js"></script>
+    <script src="/assets/main/js/jquery.slicknav.min.js"></script>
+    <script src="/assets/main/js/jquery.filterizr.min.js"></script>
+    <script src="/assets/main/js/jquery.collapse.js"></script>
+    <script src="/assets/main/js/custom.js"></script>
+    <!-- end footer -->
+
+</body>
+
+</html>
