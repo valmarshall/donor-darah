0e9cd42c3a77a713759ab089063586c50fc0285d
Muhammad Rivaldi <rivaldimuhammad4897@gmail.com>
2021-08-20 10:23:29 +0900

    menyelesaikan design awal halaman utama

--- app/Views/home.php
+++ app/Views/home.php
@@ -36,7 +36,7 @@
             <div class="row">
                 <div class="col-md-4 col-sm-12 col-xs-12">
                     <div class="logo">
-                        <a href="/">Donor</a>
+                        <a href="/"><img src="/assets/uploads/logo.png" alt="logo donor"></a>
                     </div>
                 </div>
                 <div class="col-md-8 col-sm-9">
@@ -57,12 +57,164 @@
     <!-- end navbar -->
 
     <!-- content -->
-    <div class="container">
-        <h2>halo</h2>
+
+    <!--Slider-Area Start-->
+    <div class="slider-area">
+        <div class="slider-item" style="background-image: url('/assets/uploads/banner_search.jpg')">
+            <div class="bg-3" style="opacity:0.6;"></div>
+            <div class="container">
+                <div class="row">
+                    <div class="slider-text">
+                        <h1>SEARCH A DONOR IN YOUR AREA</h1>
+                    </div>
+                    <div class="searchbox">
+
+                        <form action="#" method="post">
+
+                            <div class="col-md-3 col-sm-6">
+                                <input autocomplete="off" type="text" name="country" class="form-control" placeholder="Country Name">
+                            </div>
+
+                            <div class="col-md-3 col-sm-6">
+                                <input autocomplete="off" type="text" name="city" class="form-control" placeholder="City Name">
+                            </div>
+
+                            <div class="col-md-3 col-sm-6">
+
+                                <select data-placeholder="Choose Blood Group" class="chosen-select form-control" name="blood_group_id">
+                                    <option></option>
+                                </select>
+                            </div>
+
+                            <div class="col-md-3 col-sm-6">
+                                <input type="submit" value="Search Donor" name="form_search">
+                            </div>
+
+                        </form>
+
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
+    <!--Slider-Area End-->
+
+    <!-- Blood Group -->
+    <div class="blood-gallery bg-gray">
+        <div class="container">
+            <div class="row">
+                <div class="headline">
+                    <h2>Blood Groups</h2>
+                    <div class="headline-icon" style="background-image: url(/assets/main/img/blood.png)"></div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">A+</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">A-</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">B+</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">B-</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">AB+</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">AB-</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">O+</a>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-6 col-xs-12">
+                    <div class="blood-item">
+                        <a href="#">O-</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- end Blood Group -->
+
     <!-- end content -->
 
     <!-- footer -->
+
+    <!--Footer-Area Start-->
+    <div class="footer-area">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-3 col-sm-3">
+                    <div class="footer-item footer-service">
+                        <h2>Latest News</h2>
+                        <ul class="fmain">
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, rem!</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+                            <li>Lorem ipsum dolor sit amet.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="footer-item footer-service">
+                        <h2>Popular News</h2>
+                        <ul class="fmain">
+                            <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, rem!</li>
+                            <li>Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+                            <li>Lorem ipsum dolor sit amet.</li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="footer-item footer-service">
+                        <h2>Contact</h2>
+                        <ul>
+                            <li>Address: </li>
+                            <li>Email: </li>
+                            <li>Phone: </li>
+                            <li>Fax: </li>
+                        </ul>
+                    </div>
+                </div>
+                <div class="col-md-3 col-sm-3">
+                    <div class="footer-item footer-service">
+                        <h2>Social Media</h2>
+                        <div class="footer-social-link">
+                            <ul>
+                                <li>Lorem ipsum dolor sit amet consectetur adipisicing elit. Beatae, rem!</li>
+                                <li>Lorem, ipsum dolor sit amet consectetur adipisicing.</li>
+                                <li>Lorem ipsum dolor sit amet.</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row">
+                <div class="copyright">
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Footer-Area End-->
+
+    <!-- JS -->
     <script src="/assets/main/js/jquery-2.2.4.min.js"></script>
     <script src="/assets/main/js/jquery-ui.js"></script>
     <script src="/assets/main/js/bootstrap.min.js"></script>
@@ -75,6 +227,8 @@
     <script src="/assets/main/js/jquery.filterizr.min.js"></script>
     <script src="/assets/main/js/jquery.collapse.js"></script>
     <script src="/assets/main/js/custom.js"></script>
+    <!-- end JS -->
+
     <!-- end footer -->
 
 </body>
